3b5a5bcacfd23381e3b274b3dc5fbc7f097cfa65
Quentin <quentin.macq@outlook.fr>
2022-11-01 14:24:06 +0100

    fix: some img path not working

--- src/composables/image-path.js
+++ src/composables/image-path.js
@@ -1,7 +1,7 @@
 import { computed } from 'vue';
 
 export function useImagePath({ directory, image }) {
-  const location = `/public/assets/img/${image}`;
+  const location = `/assets/img/${directory}/${image}`;
   const path = computed(() => new URL(location, import.meta.url).href);
 
   return { path };
