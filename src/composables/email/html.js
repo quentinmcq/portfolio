ed94e3dc8a81646e4ec24f55ecfc34e474b7ead9
Quentin <quentin.macq@outlook.fr>
2023-02-24 18:38:25 +0100

    fix: yarn version and loading send form button

--- src/composables/email/html.js
+++ src/composables/email/html.js
@@ -1,4 +1,4 @@
-export function useHtml({ selector = '#message-status', html }) {
+export function useHtml({ selector = '#message-status .v-btn__content', html }) {
   const querySelector = document.querySelector(selector);
   const setHtml = (querySelector.innerHTML = html);
 
