11bd650ed6dc3404b2481be4147f88bb15ab32a3
Quentin Macq <quentin.macq@outlook.fr>
2023-12-03 12:08:44 +0100

    feat: add tsconfig.json to remove warnings on vite alias

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,7 @@
-import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
+import path from "path";
 
 export default defineConfig({
   define: {
@@ -12,8 +12,8 @@ export default defineConfig({
   plugins: [vue(), vuetify({ autoImport: true })],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   css: {
     preprocessorOptions: {
