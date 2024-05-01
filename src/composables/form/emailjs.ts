f8f97d084ba193f911694e4841d3e2aad6d819a8
Quentin Macq <quentin.macq@outlook.fr>
2024-05-01 17:44:52 +0200

    chore(emailjs): switch from deprecated emailjs-com package to @emailjs/browser

--- src/composables/form/emailjs.ts
+++ src/composables/form/emailjs.ts
@@ -1,12 +1,12 @@
 import type { Ref } from 'vue'
 
-import emailJS from 'emailjs-com'
+import emailjs from '@emailjs/browser'
 
 const USER_ID: string = import.meta.env.VITE_APP_EMAILJS_USER_ID
 const SERVICE_ID: string = import.meta.env.VITE_APP_EMAILJS_SERVICE_ID
 const TEMPLATE_ID: string = import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID
 
 export async function useEmailJs(form: Ref<HTMLFormElement | null>): Promise<void> {
-  emailJS.init(USER_ID)
-  await emailJS.sendForm(SERVICE_ID, TEMPLATE_ID, form.value?.$el)
+  emailjs.init(USER_ID)
+  await emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.value?.$el)
 }
