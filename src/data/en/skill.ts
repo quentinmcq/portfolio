a94f9676ff452899d3e4b91bc56a4add1929637a
Quentin Macq <quentin.macq@outlook.fr>
2025-07-20 19:22:51 +0200

    feat: modify en skills version

--- src/data/en/skill.ts
+++ src/data/en/skill.ts
@@ -1,32 +1,26 @@
 export default [
   {
-    color: '#ec8d0a',
-    content: ['TS', 'PHP'],
+    content: ['TypeScript', 'PHP'],
     label: 'Web languages',
   },
   {
-    color: '#2196F3',
-    content: ['MySQL'],
-    label: 'Database',
+    content: ['Git', 'Jenkins', 'Docker'],
+    label: 'Continuous integration',
   },
   {
-    color: '#4FC08D',
     content: ['Nuxt', 'Vuetify'],
     label: 'Front-end frameworks',
   },
   {
-    color: '#9C27b0',
     content: ['Symfony'],
     label: 'Back-end frameworks',
   },
   {
-    color: '#da4e76',
-    content: ['Git', 'Docker', 'Jenkins'],
-    label: 'Continuous integration',
+    content: ['Vite', 'Vitest', 'PNPM', 'ESLint', 'Pinia'],
+    label: 'Front-end tools',
   },
   {
-    color: '#e01f0f',
-    content: ['Grafana', 'RabbitMQ', 'Elasticsearch'],
-    label: 'Data management',
+    content: ['Grafana', 'RabbitMQ'],
+    label: 'Back-end tools',
   },
 ]
